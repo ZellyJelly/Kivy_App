2541db8fed42ed1c06b0407c309475b6288f1f9e
zellyjelly <westzelly@gmail.com>
2024-04-15 22:58:41 -0400

    Began learning about layouts, current layout is a scroll view. Also created a for loop that generates buttons.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from kivy.app import App 
 from kivy.uix.widget import Widget
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.button import Button
+from kivy.metrics import dp
 
-class FirstWidget(Widget):
-    pass
+
+class NewLayout(StackLayout):
+    def __init__ (self, **kwargs):
+    
+        super().__init__(**kwargs)
+        for button in range(1, 100):
+            b_button = Button(text=str(button), size_hint = (None, None), size = (dp(100), dp(100)))
+            self.add_widget(b_button)
+        
+
 
 class MyApp(App):
     pass
-
 
 
 MyApp().run()
