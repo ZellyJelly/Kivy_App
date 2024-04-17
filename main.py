ca4de3711c0ce17f53d814ed1fe9a60fcdf5479e
zellyjelly <westzelly@gmail.com>
2024-04-17 00:13:52 -0400

    Just learning Kivy

--- main.py
+++ main.py
@@ -5,24 +5,44 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.metrics import dp
+from kivy.uix.switch import Switch
 
 
 class WidgetsExample(GridLayout):
     number = 1
-    state = "normal"
+    c_state = BooleanProperty(False)
     my_text = StringProperty(str(number))
 
     def change_text(self):
-        if self.state == "down":
+        if self.c_state == True:
             self.number = self.number + 1
             self.my_text = (str(self.number))
         else:
             pass
         
     def toggle_counter(self, value):
-        self.state = value.state
+        if value.state == "down":
+            self.c_state = True
+        else:
+            self.c_state = False
+
+    def light_switch(self, value):
+        self.god_switch(self, value)
+        print(value.active)
+    
+    def god_switch(self, self_self, state):
+            if state.active == True:
+                new_button = Button(text="hi")
+                self.add_widget(new_button)
+                
+    
+            else:
+
+                self.remove_widget()
+            
+
         
 
 class NewLayout(StackLayout):
